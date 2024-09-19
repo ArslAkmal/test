7050fbdf2a3454ba92981b673babf4888411e5e8
ArslAkmal <arslan.akmal@arhamsoft.org>
2024-09-19 20:13:09 +0500

    test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path=require("path");
 
 
 app.get("/test",(req,res)=>{
-    res.json({message:"Hello From Server"})
+    res.json({message:"Hello From Server.This is a test!"})
 })
 
 // Serve admin build
